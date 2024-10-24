4f8c280ab2c2687f2286e7c0932e0aa719373e1e
Jaiden <38144027+jaidenlab@users.noreply.github.com>
2024-10-24 00:22:31 +0000

    Implement class for handling Tuya API

--- tests/test_tuya_functional.py
+++ tests/test_tuya_functional.py
@@ -2,12 +2,12 @@
 
 import os
 
-from custom_components.tuya_vacuum_maps.tuya import tuya_request
+from custom_components.tuya_vacuum_maps.tuya import TuyaCloudAPI
 
 from dotenv import load_dotenv
 
 
-def test_tuya_request_token():
+def test_tuya_request():
     """Functionally test the tuya_request."""
 
     # Load environment variables
@@ -16,9 +16,12 @@ def test_tuya_request_token():
     # Get environment variables
     CLIENT_ID = os.environ["CLIENT_ID"]
     CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+    DEVICE_ID = os.environ["DEVICE_ID"]
 
-    BASE = "https://openapi.tuyaeu.com"
-    ENDPOINT = "/v1.0/token?grant_type=1"
-    response = tuya_request(BASE, ENDPOINT, CLIENT_ID, CLIENT_SECRET)
+    BASE = "https://openapi.tuyaus.com"
+    ENDPOINT = f"/v1.0/users/sweepers/file/{DEVICE_ID}/realtime-map"
+    tuya = TuyaCloudAPI(base=BASE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+    response = tuya.request(ENDPOINT)
     print(response)
     assert response["success"]
